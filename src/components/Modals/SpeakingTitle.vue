2a3c62d4c8f34884593b9f27c66163d00dddf37b
Yael <yaelduckwen@gmail.com>
2020-05-16 15:57:03 -0300

    Issue #36, better modal order

--- src/components/Modals/SpeakingTitle.vue
+++ src/components/Modals/SpeakingTitle.vue
@@ -1,41 +1,25 @@
 <template>
     <div class="card-content">
-        <div class="media">
-            <div class="media-left">
-                
-            </div>
-            <div v-if="speaker && speakingData" class="media-content">
-                <div class="columns">
-                    <div class="column is-two-third">
-                        <div class="columns">
-                            <div class="column is-one-fifth">
-                                <img class="speaker-logo" :src="host + speaker.photo" alt="">
-                            </div>
-                            <div class="column is-four-fifth">
-                                <p class="title is-4">
-                                    <span
-                                        class="click"
-                                        @click="$root.tryBusinessCard(speaker)"
-                                    >{{speaker.name}} </span>
-                                    <span
-                                        class="click"
-                                        @click="$root.navToPage(speaker.company)"
-                                    >{{speaker.role + " " + speaker.company}}</span>
-                                </p>
-                                <p class="speaking-theme is-6">{{speakingData.theme}}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="column is-one-third">
-                        <div v-if="speakingData" class="content">
-                            <div>{{new Date(speakingData.time).toLocaleString()}}</div>
-                        </div>
-                    </div>
-                </div>
+        <div class="media speaking-media">
+            <figure class="media-left">
+                <p class="image is-64x64">
+                    <img class="speaker-logo" :src="host + speaker.photo">
+                </p>
+            </figure>
+            <div class="media-content" v-if="speaker && speakingData">
+                <p>
+                    <strong class="click" @click="$root.tryBusinessCard(speaker)" >{{speaker.name}}</strong>
+                    <small v-if="speakingData">{{new Date(speakingData.time).toLocaleString()}}</small><br>
+                    <i class="click" @click="$root.navToPage(speaker.company)" >{{speaker.role + " " + speaker.company}}</i>
+                </p>
+                <p>
+                    <small>{{speakingData.theme}}</small>
+                </p>
             </div>
         </div>
     </div>
 </template>
+
 <script>
 import {host} from '@/env'
 

80d0be75b6bcd40f8d1706e7340c12dca90359e5
Yael <yaelduckwen@gmail.com>
2020-05-22 00:43:02 -0300

    new order

--- src/components/Demoday/StartupsDemoDay.vue
+++ src/components/Demoday/StartupsDemoDay.vue
@@ -10,23 +10,11 @@
                 <div class="startup-brand">
                     <div class="columns is-multiline">
                         <div class="column">
-                            <Fundbrand fundName="UVECON"/>
-                        </div>
-                        <div class="column">
-                            <Fundbrand fundName="MASTER VENTURES" />
-                        </div>
-                        <div class="column">
-                            <Fundbrand fundName="IBC MEDIA"/>
+                            <Fundbrand fundName="COINSILLIUM" />
                         </div>
                         <div class="column">
                             <Fundbrand fundName="BLOCKCHAIN VALLEY VENTURES"/>
                         </div>
-                        <div class="column">
-                            <Fundbrand fundName="DRAPER CYGNUS" />
-                        </div>
-                        <div class="column">
-                            <Fundbrand fundName="COINSILLIUM" />
-                        </div>                        
                         <div class="column">
                             <Fundbrand fundName="CVVC" />
                         </div>
@@ -34,13 +22,25 @@
                             <Fundbrand fundName="ANGEL VEST" />
                         </div>
                         <div class="column">
-                            <Fundbrand fundName="SANCTUM"/>
+                            <Fundbrand fundName="IBC MEDIA"/>
                         </div>
                         <div class="column">
                             <Fundbrand fundName="VU VENTURE PARTNERS"/>
                         </div>
                         <div class="column">
+                            <Fundbrand fundName="DRAPER CYGNUS" />
+                        </div>
+                        <div class="column">
+                            <Fundbrand fundName="MASTER VENTURES" />
+                        </div>
+                        <div class="column">
+                            <Fundbrand fundName="SANCTUM"/>
+                        </div>
+                        <div class="column">
                             <Fundbrand fundName="FAS"/>
+                        </div>
+                        <div class="column">
+                            <Fundbrand fundName="UVECON"/>
                         </div>
                     </div>
 
@@ -52,22 +52,16 @@
                     <div class="tile is-vertical is-5 startup-brand level-left">
                         <div v-if="$root.Startups" class="tile">
 
-                            <div class="tile is-vertical is-parent">
-                                <Startuplogo id="0"/>
-                                <Startuplogo id="1"/>
-                                <Startuplogo id="2"/>
-                            </div>
-
-                            <div class="tile is-parent is-vertical">
-                                <Startuplogo id="3"/>
-                                <Startuplogo id="6"/>
-                                <Startuplogo id="7"/>
-                            </div>
-
-                            <div class="tile is-parent is-vertical">
-                                <Startuplogo id="4"/>
-                                <Startuplogo id="5"/>
-                                <Startuplogo id="8"/>
+                            <div class="columns is-multiline"> 
+                                <Startuplogo id="0"/><!-- money on change -->
+                                <Startuplogo id="5"/><!-- decrypto -->
+                                <Startuplogo id="6"/><!-- crypttp -->
+                                <Startuplogo id="1"/><!-- sensitrust -->
+                                <Startuplogo id="2"/><!-- altvesto -->
+                                <Startuplogo id="3"/><!-- block sdk -->
+                                <Startuplogo id="7"/><!-- name-->
+                                <Startuplogo id="8"/><!-- name-->
+                                <Startuplogo id="4"/><!-- makeAvent -->                                
                             </div>
 
                         </div>
@@ -81,59 +75,59 @@
                                 </div>
                             </div>
                             <div
-                                v-on:click="$router.push(`/${$root.token}/investors`)"
-                                class="tile is-child box click has-background-light investors"
+                            v-on:click="$router.push(`/${$root.token}/investors`)"
+                            class="tile is-child box click has-background-light investors"
                             >
-                                <div class="has-text-centered">
-                                    <h3>{{content.investors}}</h3>
-                                </div>
+                            <div class="has-text-centered">
+                                <h3>{{content.investors}}</h3>
                             </div>
-                        </div>  
-                    </div>
+                        </div>
+                    </div>  
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     </div>
+</div>
 </template>
 <script>
-import {host} from '@/env'
-import Fundbrand from './Fundbrand'
-import Startuplogo from './Startuplogo'
+    import {host} from '@/env'
+    import Fundbrand from './Fundbrand'
+    import Startuplogo from './Startuplogo'
 
-export default {
-    components: {
-        Fundbrand,
-        Startuplogo
-    },
-    data() {
-        this.content = this.$root.content.StartupsDemoDay
+    export default {
+        components: {
+            Fundbrand,
+            Startuplogo
+        },
+        data() {
+            this.content = this.$root.content.StartupsDemoDay
 
-        this.$root.checkComponentAccess('startupdemoday')
-        .then(res => {
-            if (res)
-                this.ready = true
-            else
-                this.$root.createError(this.$root.content.ErrorMessages[3], 'oops')
-        })
-
-        return {
-            ready: false,
-
-            host: host,
-            show: true,
-            content: this.content
-        }
-    },
-    methods: {
-        goToPitchingBooth () {
-            this.$root.checkComponentAccess('startupdemodaypitchingbooth')
+            this.$root.checkComponentAccess('startupdemoday')
             .then(res => {
                 if (res)
-                    this.$router.push(`/${this.$root.token}/ddpb`)
+                    this.ready = true
                 else
-                    this.$root.createError(this.$root.content.ErrorMessages[5], 'oops')
+                    this.$root.createError(this.$root.content.ErrorMessages[3], 'oops')
             })
-        }
-    },
-}
+
+            return {
+                ready: false,
+
+                host: host,
+                show: true,
+                content: this.content
+            }
+        },
+        methods: {
+            goToPitchingBooth () {
+                this.$root.checkComponentAccess('startupdemodaypitchingbooth')
+                .then(res => {
+                    if (res)
+                        this.$router.push(`/${this.$root.token}/ddpb`)
+                    else
+                        this.$root.createError(this.$root.content.ErrorMessages[5], 'oops')
+                })
+            }
+        },
+    }
 </script>

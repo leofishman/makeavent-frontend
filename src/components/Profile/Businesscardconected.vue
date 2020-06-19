d4d5688a3ec826c83e28717115962b493f03d397
Yael <yaelduckwen@gmail.com>
2020-06-19 15:34:19 -0300

    New brand colors Edit in profile look Business card semantic fix

--- src/components/Profile/Businesscardconected.vue
+++ src/components/Profile/Businesscardconected.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="businesscardconected">
-		<h3 class="click" v-on:click="$root.tryBusinessCard(data)">{{data.name}}</h3>
-		<h3 class="click has-text-grey" v-on:click="openCompany()">{{data.role}} {{content.at}} {{data.company}}</h3>
+		<p>
+			<strong class="click" v-on:click="$root.tryBusinessCard(data)">{{data.name}}</strong>
+			<span class="click has-text-grey" v-on:click="openCompany()">{{data.role}} {{content.at}} {{data.company}}</span>
+		</p>
 		<ul>
 			<li v-if="canCall" v-on:click="$root.privateCall(data)"><img src="@/assets/icon/icon-call.svg"></li>
 			<!-- <li><img src="@/assets/icon/icon-mail.svg"></li> -->
@@ -12,29 +14,29 @@
 
 <script>
 
-export default {
-	name: "Businesscardconected",
-	props: {
-		data: Object
-	},
-	data() {
-		this.$root.canCall(this.data)
-		.then(res => {
-			this.canCall = res
-		})
+	export default {
+		name: "Businesscardconected",
+		props: {
+			data: Object
+		},
+		data() {
+			this.$root.canCall(this.data)
+			.then(res => {
+				this.canCall = res
+			})
 
-        return {
-			content: this.$root.content.common,
-			canCall: this.canCall
-        }
-    },
-    methods: {
-		openCompany () {
-			this.$root.defineBoothType(this.data.company)
-			.then(type => { this.$root.openPage(type, this.data.company) })
-		}
-	},
-}
+			return {
+				content: this.$root.content.common,
+				canCall: this.canCall
+			}
+		},
+		methods: {
+			openCompany () {
+				this.$root.defineBoothType(this.data.company)
+				.then(type => { this.$root.openPage(type, this.data.company) })
+			}
+		},
+	}
 </script>
 
 <style scoped>
--- src/components/Profile/Businesscardpending.vue
+++ src/components/Profile/Businesscardpending.vue
@@ -1,8 +1,10 @@
 <template>
 	<div class="businesscardpending">
 		<b-loading :is-full-page="false" :active.sync="isLoading" :can-cancel="false"></b-loading>
-		<h3>{{data.name}}</h3>
-		<h3 class="has-text-grey-light">{{data.role}} {{common.at}} {{data.company}}</h3>
+		<p>
+			<strong>{{data.name}}</strong>
+			<span class="has-text-grey">{{data.role}} {{common.at}} {{data.company}}</span>
+		</p>
 		<ul>
 			<li v-on:click="showLoaderAndSendPost()"><img src="@/assets/icon/icon-check.svg"></li>
 			<li v-on:click="showLoaderAndDeny()"><img src="@/assets/icon/icon-close.svg"></li>
@@ -13,36 +15,36 @@
 
 <script>
 
-export default {
-	name: "Businesscardpending",
+	export default {
+		name: "Businesscardpending",
 
-	props: {
-		data: Object
-	},
-	data () {
-		window.EventBus.$on('close-overlay-by-id', (id) => {
-			if (compare(this.data._id, id))
-				this.isLoading = false
-		})
-
-		return {
-			isLoading: false,
-			loadingComponents: [],
-			common: this.$root.content.common
-		}
-	},
-	methods: {
-		showLoaderAndSendPost () {
-			this.isLoading = true
-			this.$root.acceptBusinessCard(this.data)
+		props: {
+			data: Object
 		},
+		data () {
+			window.EventBus.$on('close-overlay-by-id', (id) => {
+				if (compare(this.data._id, id))
+					this.isLoading = false
+			})
 
-		showLoaderAndDeny () {
-			this.isLoading = true
-			this.$root.denyBusinessCardRequest(this.data)
-		}
-	},
-}
+			return {
+				isLoading: false,
+				loadingComponents: [],
+				common: this.$root.content.common
+			}
+		},
+		methods: {
+			showLoaderAndSendPost () {
+				this.isLoading = true
+				this.$root.acceptBusinessCard(this.data)
+			},
+
+			showLoaderAndDeny () {
+				this.isLoading = true
+				this.$root.denyBusinessCardRequest(this.data)
+			}
+		},
+	}
 </script>
 
 <style scoped>

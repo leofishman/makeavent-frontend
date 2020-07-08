f7c8afcdd425b23921e4c576f18439faa75faeee
Yael <yaelduckwen@gmail.com>
2020-07-08 14:11:52 -0300

    modal for materials init

--- src/components/CompanyProfile/CompanyNew.vue
+++ src/components/CompanyProfile/CompanyNew.vue
@@ -1,201 +1,225 @@
 <template>   
-    <div v-if="ready" id="profile-company-new" :class="sponsorClass" :style="{ backgroundImage: `url('${host + bg}')` }">
-        <video v-if="video" class="bg-video" autoplay muted loop>
-            <source :src="host + video" type="video/mp4">
-            </video>
+	<div v-if="ready" id="profile-company-new" :class="sponsorClass" :style="{ backgroundImage: `url('${host + bg}')` }">
+		<video v-if="video" class="bg-video" autoplay muted loop>
+			<source :src="host + video" type="video/mp4">
+			</video>
 
-            <navbar></navbar>
+			<navbar></navbar>
 
-            <!-- NOT RELEASED -->
-            <!-- <Storycreate v-if="$root.isAdmin(contacts)"/> -->
+			<!-- NOT RELEASED -->
+			<!-- <Storycreate v-if="$root.isAdmin(contacts)"/> -->
 
-            <div class="container">
-                <section class="section section-profile-company">
-                    <div class="columns is-desktop">
-                        <aside class="column is-full-tablet is-half-widescreen is-one-third-fullhd">
-                            <div class="profile-top">
-                                <figure class="company-logo">
-                                    <img :src="this.logo">
-                                </figure>
+			<div class="container">
+				<section class="section section-profile-company">
+					<div class="columns is-desktop">
+						<aside class="column is-full-tablet is-half-widescreen is-one-third-fullhd">
+							<div class="profile-top">
+								<figure class="company-logo">
+									<img :src="this.logo">
+								</figure>
 
-                                <ul class="list-network">
-                                    <li v-if="website" v-on:click="openAndTrack(website)">
-                                        <span>
-                                            <img src="@/assets/icon/icon-email.svg" :alt="commonContent.Website">
-                                        </span>
-                                    </li>
-                                    <li v-for="(el, key, index) in socials" :key="index" v-on:click="openAndTrack(el)">
-                                        <span v-if="el">
-                                            <img :src="require(`../../assets/icon/icon-${key.toLowerCase()}.svg`)" :alt="key">
-                                        </span>
-                                    </li>
-                                </ul>
-                                <div class="company-demo">
-                                    <button v-if="demo" class="play-demo button is-primary" @click="demoModalActive = true">{{content.viewDemo}}</button>
+								<ul class="list-network">
+									<li v-if="website" v-on:click="openAndTrack(website)">
+										<span>
+											<img src="@/assets/icon/icon-email.svg" :alt="commonContent.Website">
+										</span>
+									</li>
+									<li v-for="(el, key, index) in socials" :key="index" v-on:click="openAndTrack(el)">
+										<span v-if="el">
+											<img :src="require(`../../assets/icon/icon-${key.toLowerCase()}.svg`)" :alt="key">
+										</span>
+									</li>
+								</ul>
+								<div class="company-demo">
+									<button v-if="demo" class="play-demo button is-primary" @click="demoModalActive = true">{{content.viewDemo}}</button>
 
-                                    <b-modal :active.sync="demoModalActive"
-                                    has-modal-card
-                                    trap-focus
-                                    :destroy-on-hide="false"
-                                    aria-role="dialog"
-                                    aria-modal>
-                                    <DemoModal />
-                                    
-                                </b-modal>
-                            </div>
-                        </div>
+									<b-modal
+									:active.sync="demoModalActive"
+									has-modal-card
+									trap-focus
+									:destroy-on-hide="false"
+									aria-role="dialog"
+									aria-modal>
+									<DemoModal/></b-modal>
+								</div>
+							</div>
 
-                        <div class="profile-bottom">
-                            <div v-html="description" class="profile-bio"></div>
+							<div class="profile-bottom">
+								<div class="profile-bio">
+									<p v-html="description" class="bio-content"></p>
+									<button class="button is-success is-fullwidth" v-on:click="bioExpand" >Expand</button>
+								</div>
 
-                            <PitchDeck v-for="(el, index) in materials" :key="index" :data="el"/>
-                        </div>
-                    </aside>
+								<div class="company-materials">
+									<button v-if="materials" class="play-materials button is-primary" @click="materialsModalActive = true">{{content.viewDemo}}</button>
 
-                    <div class="column">
-                        <div class="profile-stream box">
-                            <!-- [YD] stream here -->
-                        </div>
+									<b-modal
+									:active.sync="materialsModalActive"
+									has-modal-card
+									trap-focus
+									:destroy-on-hide="false"
+									aria-role="dialog"
+									aria-modal>
+									<MaterialsModal/></b-modal>
+								</div>
 
-                        <div class="columns is-multiline member-clasic">
-                            <div class="column" v-for="(el, index) in contacts" :key="index">
-                                <Member :data="el"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
+								<PitchDeck v-for="(el, index) in materials" :key="index" :data="el"/>
+							</div>
+						</aside>
 
-        <!-- Profile Chat -->
-        <div id="chat" class="chat-company-profile">
-            <div class="chat-top">
-                <article>
-                    <h3>{{content.networkingRoom}}</h3>
-                    <img src="@/assets/img/join-chat.png" />
-                    <button class="button is-primary is-medium" v-on:click="openPage('NetworkingRooms')">{{content.join}}</button>
-                    <p class="has-text-success">{{content.currentlyOnline}}: ##</p>
-                </article>
-            </div>
-            <Chat :checkAccess="'companychat'" type="company" :parent="self" :name="name" />
-        </div>
-    </div>
-</template>
+						<div class="column">
+							<div class="profile-stream box">
+								<!-- [YD] stream here -->
+							</div>
 
-<script>
-    import axios from 'axios'
-    import {host} from '@/env'
-    import Storycreate from '@/components/Stories/StoryCreate.vue';
-    import socialLogos from '@/assets/img/socials'
-    import Pagetitle from '@/components/Pagetitle.vue';
-    import Member from './Member.vue';
-    import MemberCustom from './MemberCustom.vue';
-    import DemoModal from './DemoModal.vue';
-    import Chat from './Chat.vue';
-    import PitchDeck from '@/components/Demoday/Pitchdeck';
+							<div class="columns is-multiline member-clasic">
+								<div class="column" v-for="(el, index) in contacts" :key="index">
+									<Member :data="el"/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
 
-    export default {
-        props: {
-            name: {
-                type: String,
-                default: ""
-            }
-        },
-        components: {
-            Storycreate,
-            Pagetitle,
-            Member,
-            MemberCustom,
-            DemoModal,
-            Chat,
-            PitchDeck
-        },
-        data () {
-            this.chatHeight;
-            this.chatHistory = [];
-            this.userTextMessage = ""
-            this.token = this.$root.token
-            this.showMessageModal = false
-            this.chatAvailable = false
-            this.sponsor = ''
-            this.description = ""
-            this.demo = ""
-            this.website = ""
-            this.socials = ""
-            this.contacts = ""
-            this.logo = ""
-            this.bg = ""
-            this.video = ""
-            this.sponsorClass = ""
-            this.materials = ""
+			<!-- Profile Chat -->
+			<div id="chat" class="chat-company-profile">
+				<div class="chat-top">
+					<article>
+						<h3>{{content.networkingRoom}}</h3>
+						<img src="@/assets/img/join-chat.png" />
+						<button class="button is-primary is-medium" v-on:click="openPage('NetworkingRooms')">{{content.join}}</button>
+						<p class="has-text-success">{{content.currentlyOnline}}: ##</p>
+					</article>
+				</div>
+				<Chat :checkAccess="'companychat'" type="company" :parent="self" :name="name" />
+			</div>
+		</div>
+	</template>
 
-            /* wait until token and sponsors ready*/
-            this.$root.check('token Sponsors').then(() => {
-                const name = this.name.toUpperCase()
-                this.sponsor = this.$root.Sponsors.filter(el => compare(el.name, name))[0]
+	<script>
+		import axios from 'axios'
+		import {host} from '@/env'
+		import Storycreate from '@/components/Stories/StoryCreate.vue';
+		import socialLogos from '@/assets/img/socials'
+		import Pagetitle from '@/components/Pagetitle.vue';
+		import Member from './Member.vue';
+		import MemberCustom from './MemberCustom.vue';
+		import DemoModal from './DemoModal.vue';
+		import MaterialsModal from './MaterialsModal.vue';
+		import Chat from './Chat.vue';
+		import PitchDeck from '@/components/Demoday/Pitchdeck';
 
-                this.description = this.sponsor.description
-                this.demo = this.sponsor.demo
-                this.website = this.sponsor.website
-                this.socials = this.sponsor.socials
-                this.contacts = this.sponsor.contacts
-                this.bg = this.sponsor.backgroundImage
-                this.video = this.sponsor.backgroundVideo
-                this.sponsorClass = this.sponsor.sponsorClass
-                this.logo = host + this.sponsor.logo
-                this.materials = this.sponsor.materials
+		export default {
+			props: {
+				name: {
+					type: String,
+					default: ""
+				}
+			},
+			components: {
+				Storycreate,
+				Pagetitle,
+				Member,
+				MemberCustom,
+				DemoModal,
+				MaterialsModal,
+				Chat,
+				PitchDeck
+			},
+			data () {
+				this.chatHeight;
+				this.chatHistory = [];
+				this.userTextMessage = ""
+				this.token = this.$root.token
+				this.showMessageModal = false
+				this.chatAvailable = false
+				this.sponsor = ''
+				this.description = ""
+				this.demo = ""
+				this.website = ""
+				this.socials = ""
+				this.contacts = ""
+				this.logo = ""
+				this.bg = ""
+				this.video = ""
+				this.sponsorClass = ""
+				this.materials = ""
 
-                this.ready = true
-            }).catch(e => console.log(`${e} inaccessible`))
+				/* wait until token and sponsors ready*/
+				this.$root.check('token Sponsors').then(() => {
+					const name = this.name.toUpperCase()
+					this.sponsor = this.$root.Sponsors.filter(el => compare(el.name, name))[0]
 
-            return {
-                demoModalActive: false,
-                self: this,
-                content: this.$root.content.Company,
-                commonContent: this.$root.content.common,
+					this.description = this.sponsor.description
+					this.demo = this.sponsor.demo
+					this.website = this.sponsor.website
+					this.socials = this.sponsor.socials
+					this.contacts = this.sponsor.contacts
+					this.bg = this.sponsor.backgroundImage
+					this.video = this.sponsor.backgroundVideo
+					this.sponsorClass = this.sponsor.sponsorClass
+					this.logo = host + this.sponsor.logo
+					this.materials = this.sponsor.materials
 
-                ready: false,
-                getInTouch: ["Facebook", "Linkedin", "Telegram"],
+					this.ready = true
+				}).catch(e => console.log(`${e} inaccessible`))
 
-                userTextMessage: this.userTextMessage,
-                chatHeight: this.chatHeight,
-                chatHistory: this.chatHistory,
-                showMessageModal: this.showMessageModal,
+				return {
+					demoModalActive: false,
+					materialsModalActive: false,
+					self: this,
+					content: this.$root.content.Company,
+					commonContent: this.$root.content.common,
 
-                showQuote: false,
-                quotedMessage: '',
-                quotedName: '',
-                quoteId: '',
+					ready: false,
+					getInTouch: ["Facebook", "Linkedin", "Telegram"],
 
-                logo: this.logo,
-                description: this.description,
-                demo: this.demo,
-                website: this.website,
-                socials: this.socials,
-                socialLogos: socialLogos,
-                contacts: this.contacts,
+					userTextMessage: this.userTextMessage,
+					chatHeight: this.chatHeight,
+					chatHistory: this.chatHistory,
+					showMessageModal: this.showMessageModal,
 
-                host: host,
-                bg: this.bg,
-                video: this.video,
-                sponsorClass: this.sponsorClass,
+					showQuote: false,
+					quotedMessage: '',
+					quotedName: '',
+					quoteId: '',
 
-                chat: "",
+					logo: this.logo,
+					description: this.description,
+					demo: this.demo,
+					website: this.website,
+					socials: this.socials,
+					socialLogos: socialLogos,
+					contacts: this.contacts,
 
-                chatAvailable: this.chatAvailable,
-                materials: this.materials
-            }
-        },
-        methods: {
-            openAndTrack (link) {
-                this.$root.track(name, link)
-                this.$root.openExternalInBlank(link)
-            },
-            openPage (name) {
-                this.$router.push(`/${this.$root.$router.currentRoute.params.id}/${name}`).catch(() => {
-                    window.location.reload()
-                })
-            },
-        },
-    }
-</script>
+					host: host,
+					bg: this.bg,
+					video: this.video,
+					sponsorClass: this.sponsorClass,
+
+					chat: "",
+
+					chatAvailable: this.chatAvailable,
+					materials: this.materials
+				}
+			},
+			methods: {
+				openAndTrack (link) {
+					this.$root.track(name, link)
+					this.$root.openExternalInBlank(link)
+				},
+				openPage (name) {
+					this.$router.push(`/${this.$root.$router.currentRoute.params.id}/${name}`).catch(() => {
+						window.location.reload()
+					})
+				},
+				bioExpand: function() {
+					this.$emit('onOpen');
+				},
+				bioCollapse: function() {
+					this.$emit('onClose');
+				}
+			},
+		}
+	</script>
---
+++ src/components/CompanyProfile/MaterialsModal.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="modal-card demo-modal">
+		<header class="modal-card-head">
+			<p class="modal-card-title">Materials</p>
+		</header>
+		<section class="modal-card-body">
+			<PitchDeck v-for="(el, index) in materials" :key="index" :data="el"/>
+		</section>
+	</div>
+</template>
+<script>
+	import PitchDeck from '@/components/Demoday/Pitchdeck';
+	export default {
+		name: "MaterialsModal",
+		components: {
+			PitchDeck
+		},
+		data() {
+			return {
+				content: this.$root.content.Company,
+				
+				isDemoModalActive: false,
+
+				demos: [
+				{ name: 'Future mix', url: 'https://www.youtube.com/embed/nMcb7bS-I88?controls=0' },
+				{ name: 'This is another mix', url: 'https://www.youtube.com/embed/nMcb7bS-I88?controls=0' },
+				{ name: 'And another', url: 'https://www.youtube.com/embed/nMcb7bS-I88?controls=0' },
+				{ name: 'The last one', url: 'https://www.youtube.com/embed/nMcb7bS-I88?controls=0' },
+				{ name: 'This is another mix', url: 'https://www.youtube.com/embed/nMcb7bS-I88?controls=0' },
+				]
+			}
+		},
+		methods: {
+			getThumbnail (url) {
+				return `https://img.youtube.com/vi/${url.split("embed/")[1].split("?")[0]}/0.jpg`
+			},
+
+			openVideoModal (url) {
+				this.$buefy.modal.open({
+					fullScreen: true,
+					hasModalCard: false,
+					canCancel: true,
+					trapFocus: true,
+					customClass: 'modal-dark fullscreen-iframe',
+					component: {
+						template: `<iframe src="${url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+					}
+				})
+			}
+		},
+	}
+</script>
+<style lang="scss">
+@import "@/assets/css/variables.scss";
+
+.demo-video-card {
+	min-width: 200px;
+	max-width: 200px;
+
+	&:hover {
+		box-shadow: $navbar-box-shadow-size;
+	}
+}
+
+.modal-dark .modal-content{
+	background-color: $light-invert !important;
+}
+
+.fullscreen-iframe {
+	iframe {
+		display: block;
+		width:95%;
+		height:95%;
+		margin:auto;
+	}
+}
+</style>

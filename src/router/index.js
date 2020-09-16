2e20c527536bec1781b333800d09d04aa4083f4c
Bogdan <sizov.workingbox@gmail.com>
2020-09-17 00:00:21 +0200

    temp solution for fas meetup

--- src/router/index.js
+++ src/router/index.js
@@ -194,6 +194,14 @@ function isMeetupWithId (to) {
 }
 
 router.beforeEach((to, from, next) => {
+    if (to.query.id == "5f53a682c810aa9f7a8150bc" && to.path == "/meetup") {
+        window.location.href = '/fas_futureblock?id=5f53a682c810aa9f7a8150bc'
+    }
+
+    if (to.query.id == "5f53a682c810aa9f7a8150bc" && to.path == "/networking") {
+        window.location.href = '/fas_futureblock_networking?id=5f53a682c810aa9f7a8150bc'
+    }
+    
     if (to.meta.requiresAuth) {
         if (localStorage.auth) {
             axios.get(api + `/auth/checkAccess?path=${toUp(to.name)}`, {
